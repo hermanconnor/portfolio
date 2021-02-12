63f30e753115c10d5b5673c4dd7fb7f7d47a5815
Herman <hermanconnor11@gmail.com>
2021-02-12 13:40:05 -0500

    Add error pages

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,17 @@ router.get('/about', (req, res) => {
   res.render('about');
 });
 
+/* GET generated error route - create and throw 500 server error */
+router.get('/error', (req, res, next) => {
+  // Log out custom error handler indication
+  console.log('Custom error route called');
+
+  const err = new Error();
+  // err.message = `Custom 500 error thrown`;
+  err.status = 500;
+  throw err;
+});
+
 router.get('/project/:id', (req, res, next) => {
   const projectID = req.params.id;
   const project = projects.find(({ id }) => id === +projectID);
