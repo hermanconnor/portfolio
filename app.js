9d94c0af06b166b5a0088cd171d4ac5270a8f350
Herman <hermanconnor11@gmail.com>
2021-02-16 13:42:44 -0500

    Add keyframes to css

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   next(err);
 });
 
-/* Global error handler */
+/* GLOBAL ERROR HANDLER */
 app.use((err, req, res, next) => {
   if (err.status === 404) {
     res.status(404).render('page-not-found', { err });
@@ -31,17 +31,5 @@ app.use((err, req, res, next) => {
     res.render('error', { err });
   }
 });
-
-// app.use((req, res, next) => {
-//   const err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// app.use((err, req, res, next) => {
-//   res.locals.error = err;
-//   res.status(err.status);
-//   res.render('error', err);
-// });
 
 app.listen(3000, () => console.log('Server running on port 3000.'));
--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,18 @@ const express = require('express');
 const router = express.Router();
 const { projects } = require('../data/data.json');
 
+// Home Route
 router.get('/', (req, res) => {
   res.locals = projects;
   res.render('index', { projects });
 });
 
+// About Route
 router.get('/about', (req, res) => {
   res.render('about');
 });
 
-/* GET generated error route - create and throw 500 server error */
-router.get('/error', (req, res, next) => {
-  // Log out custom error handler indication
-  console.log('Custom error route called');
-
-  const err = new Error();
-  // err.message = `Custom 500 error thrown`;
-  err.status = 500;
-  throw err;
-});
-
+// Project Routes
 router.get('/project/:id', (req, res, next) => {
   const projectID = req.params.id;
   const project = projects.find(({ id }) => id === +projectID);
@@ -34,6 +26,17 @@ router.get('/project/:id', (req, res, next) => {
     err.message = `Looks like the project you requested doesn't exist`;
     next(err);
   }
+});
+
+/* GET generated error route - create and throw 500 server error */
+router.get('/error', (req, res, next) => {
+  // Log out custom error handler indication
+  console.log('Custom error route called');
+
+  const err = new Error();
+  // err.message = `Custom 500 error thrown`;
+  err.status = 500;
+  throw err;
 });
 
 module.exports = router;
